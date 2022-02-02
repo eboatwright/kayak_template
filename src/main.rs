f95da27fcdb52f4f5c20d0b1d26c3ada8ec85556
eboatwright <eboatwright7236@gmail.com>
2022-02-02 13:51:42 -0600

    2/2/22 1:51 PM
     - added GameState
     - added Resources
     - added main
     - added macroquad initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello World!");
+use macroquad::prelude::*;
+use rutoe::*;
+
+#[macroquad::main(|| {
+    Conf {
+        window_title: "RUTOE Template".to_string(),
+        window_width: 960,
+        window_height: 600,
+        window_resizable: true,
+        ..Default::default()
+    }
+})]
+async fn main() {
+    let mut master = Master::default();
+        state: Box::new(GameState {
+        }),
+        resources: Box::new(Resources {
+        }),
+    };
+
+    start(&mut master).await;
 }
+
+struct GameState {
+}
+
+impl State for GameState {
+    fn initialize(&mut self) {
+    }
+
+    fn update(&mut self) {
+    }
+
+    fn render(&self, resources: &Box<dyn ResourceContainer>) {
+        let resources: &Resources = convert_resources(resources);
+    }
+}
+
+struct Resources {
+}
+impl ResourceContainer for Resources {}
